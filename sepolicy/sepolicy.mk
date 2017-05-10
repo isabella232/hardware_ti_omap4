Unlegacy-Android/android_hardware_ti_omap4

omap4: sepolicy: fix 4.4 compatibility
- gpu_device type exists in LP and newer, which breaks compiling on KitKat
- Support asec containers getting mounted on KitKat

Change-Id: I77c24fa34e5f2425bc2ce3940a41e6db0c7777fb

sepolicy/sepolicy.mk
@@ -1,0 +1,18 @@
+OMAP4_SEPOLICY_BASE := hardware/ti/omap4/sepolicy
+
+# 4.4
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -eq 19 || echo 1),)
+BOARD_SEPOLICY_UNION += \
+	file.te \
+	file_contexts \
+	init.te \
+	pvrsrvinit.te \
+	system_server.te
+endif
+
+# >=5.0
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 20 || echo 1),)
+BOARD_SEPOLICY_DIRS += $(OMAP4_SEPOLICY_BASE)/compat/lp
+endif
+
+BOARD_SEPOLICY_DIRS += $(OMAP4_SEPOLICY_BASE)

isabella232/hardware_ti_omap4

sepolicy/sepolicy.mk
@@ -1,0 +1,17 @@
+OMAP4_SEPOLICY_BASE := hardware/ti/omap4/sepolicy
+
+# 4.4
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -eq 19 || echo 1),)
+BOARD_SEPOLICY_UNION += \
+	file.te \
+	file_contexts \
+	init.te \
+	pvrsrvinit.te
+endif
+
+# >=5.0
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 20 || echo 1),)
+BOARD_SEPOLICY_DIRS += $(OMAP4_SEPOLICY_BASE)/compat/lp
+endif
+
+BOARD_SEPOLICY_DIRS += $(OMAP4_SEPOLICY_BASE)